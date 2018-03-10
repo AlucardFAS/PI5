64e9556659955c7d8228056aa4ac8715c1eae9dd
Victor Eleuterio Trindade <eleuteriotrindade@gmail.com>
2018-03-10 18:21:13 -0300

    Codigo traduzido!!!

--- PI V/PI V/PI_V.py
+++ PI V/PI V/PI_V.py
@@ -5,20 +5,20 @@ import random
 import operator
 
 #Carrega o arquivo CSV
-def loadDataset(filename, divide_set, trainingSet=[] , testSet=[]):
-	with open(filename, 'r') as csvfile:#Carrega como CSV File
-	    lines = csv.reader(csvfile)#Armazena os valores em linhas
-	    dataset = list(lines)#Cria uma lista
+def carregaDados(filename, divideConjunto, conjuntoTratado=[] , conjuntoTeste=[]):
+    with open(filename, 'r') as csvfile:#Carrega como CSV File
+        lines = csv.reader(csvfile)#Armazena os valores em linhas
+        dataset = list(lines)#Cria uma lista
 
         #Passa pelos n elementos do dataset
-	    for x in range(len(dataset)):
-	        for y in range(4):#le os n elementos de cada linha
-	            dataset[x][y] = float(dataset[x][y])#Gera uma matriz de elementos do dataset
-	        if random.random() < divide_set:#Randomiza os valores, ve o que esta abaixo do float enviado e armazena nos conjuntos
-	            trainingSet.append(dataset[x])
-	        else:
-	            testSet.append(dataset[x])
-	    return dataset
+        for x in range(len(dataset)):
+            for y in range(4):#le os n elementos de cada linha
+                dataset[x][y] = float(dataset[x][y])#Gera uma matriz de elementos do dataset
+            if random.random() < divideConjunto:#Randomiza os valores, ve o que esta abaixo do float enviado e armazena nos conjuntos
+                conjuntoTratado.append(dataset[x])
+            else:
+                conjuntoTeste.append(dataset[x])
+        return dataset
 
 
 
@@ -26,68 +26,66 @@ def loadDataset(filename, divide_set, trainingSet=[] , testSet=[]):
 #Recebe os conjuntos, sendo um de treino onde estão os dados com as classes, um de teste,
 #onde estão as instâncias sem a classe(que você quer saber os vizinhos mais próximos),
 #e o numero k de vizinhos a serem investigados.
-def getNeighbors(trainingSet, testInstance, k):
-	distances = []
-	length = len(testInstance)-1#porque é um vetor
+def getVizinhos(conjuntoTratado, instanciaTeste, k):
+	distancia = []
+	length = len(instanciaTeste)-1#porque é um vetor
 
 
     #Fará o calculo euclidiano de distancia entre os n valores teste 
-	for x in range(len(trainingSet)):
-		dist = euclideanDistance(testInstance, trainingSet[x], length)
-		distances.append((trainingSet[x], dist))
-	distances.sort(key=operator.itemgetter(1))#Ordena o vetor de distancia['valores', distancia] pela distancia
-	neighbors = []
+	for x in range(len(conjuntoTratado)):
+		dist = distanciaEuclidiana(instanciaTeste, conjuntoTratado[x], length)
+		distancia.append((conjuntoTratado[x], dist))
+	distancia.sort(key=operator.itemgetter(1))#Ordena o vetor de distancia['valores', distancia] pela distancia
+	vizinhos = []
 	for x in range(k):
-		neighbors.append(distances[x][0])#para o numero k de vizinhos, varre a matriz e obtém as instancias de vizinhos
-	return neighbors
+		vizinhos.append(distancia[x][0])#para o numero k de vizinhos, varre a matriz e obtém as instancias de vizinhos
+	return vizinhos
 
 
 #calculo de distancia eucliadiana
-def euclideanDistance(instance1, instance2, length):
-	distance = 0
+def distanciaEuclidiana(instancia1, instancia2, length):
+	distancia = 0
 	for x in range(length):
-		distance += pow((instance1[x] - instance2[x]), 2)
-	return math.sqrt(distance)
+		distancia += pow((instancia1[x] - instancia2[x]), 2)
+	return math.sqrt(distancia)
 
 
 #Define a classe tendo como base os vizinhos
-def getResponse(neighbors):
-	classVotes = {}#Cria um Dict(Tipo um JSON)
-	for x in range(len(neighbors)):#le ate o ultimo vizinho
-		response = neighbors[x][-1]#armazena de cada linha o ultimo valor
-		if response in classVotes:#se o valor armazenado pertence a um dict, incrementa esse valor
-			classVotes[response] += 1
+def getResposta(vizinhos):
+	classeVotos = {}#Cria um Dict(Tipo um JSON)
+	for x in range(len(vizinhos)):#le ate o ultimo vizinho
+		resposta = vizinhos[x][-1]#armazena de cada linha o ultimo valor
+		if resposta in classeVotos:#se o valor armazenado pertence a um dict, incrementa esse valor
+			classeVotos[resposta] += 1
 		else:#senão, cria um novo dict e define que existe 1 instancia
-			classVotes[response] = 1
-	sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)# Ordena os valores do maior pro menor
-	return sortedVotes[0][0]
+			classeVotos[resposta] = 1
+	votosOrdenados = sorted(classeVotos.items(), key=operator.itemgetter(1), reverse=True)# Ordena os valores do maior pro menor
+	return votosOrdenados[0][0]
 
 
 #verifica a precisao dos dados
-def getAccuracy(testSet, predictions):
-	correct = 0
-	for x in range(len(testSet)):
-		if testSet[x][-1] is predictions[x]: #se o test for igual a previsao, incrementa 1 aos corretos
-			correct += 1
-	return (correct/float(len(testSet))) * 100.0 #divide os corretos pelo total e transforma em porcentagem
+def getPrecisao(conjuntoTeste, previsoes):
+	correto= 0
+	for x in range(len(conjuntoTeste)):
+		if conjuntoTeste[x][-1] is previsoes[x]: #se o test for igual a previsao, incrementa 1 aos corretos
+			correto += 1
+	return (correto/float(len(conjuntoTeste))) * 100.0 #divide os corretos pelo total e transforma em porcentagem
 
 def main():
-    classVotes = {}
-    trainingSet=[]
-    testSet=[]
-    loadDataset('DataCSV\iris.csv', 0.66, trainingSet, testSet)
-    print ('Train: ' + repr(len(trainingSet)))
-    print ('Test: ' + repr(len(testSet)))
-
+    
+    conjuntoTratado = []
+    conjuntoTeste = []
+    #carregaDados('DataCSV\iris.csv', 0.66, trainingSet, testSet)
+    #print ('Train: ' + repr(len(trainingSet)))
+    #print ('Test: ' + repr(len(testSet)))
     #trainSet = [[2, 2, 2, 'a'], [4, 4, 4, 'b'],[3,3,3,'a']]
-    testInstance = [0.78, 0.7,0.93]
+    instanciaTeste = [0.78, 0.7,0.93]
     k = 8
-    neighbors = getNeighbors(loadDataset('DataCSV\iris.csv', 0.66, trainingSet, testSet), testInstance, k)
-    print(neighbors)
+    vizinhos = getVizinhos(carregaDados('DataCSV\iris.csv', 0.66, conjuntoTratado, conjuntoTeste), instanciaTeste, k)
+    print ('Vizinhos: %s' % vizinhos)
 
-    nyan = getResponse(neighbors)
-
-    print(nyan)
-
+    resultado = getResposta(vizinhos)
+    
+    print("Classe: %s" % resultado)
 
 main()
