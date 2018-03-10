1fc57ff14eb5e8fd9808fac81b02f5251a52a2c0
Fernando A da Silva <FernandoImp@outlook.com.br>
2018-03-10 15:12:29 -0300

    KNN funcionando mas nao generico

--- PI V/PI V/PI_V.py
+++ PI V/PI V/PI_V.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import math
 import random
 import operator
 
@@ -17,6 +18,9 @@ def loadDataset(filename, divide_set, trainingSet=[] , testSet=[]):
 	            trainingSet.append(dataset[x])
 	        else:
 	            testSet.append(dataset[x])
+	    return dataset
+
+
 
 #Acha vizinhos
 #Recebe os conjuntos, sendo um de treino onde estão os dados com as classes, um de teste,
@@ -25,6 +29,7 @@ def loadDataset(filename, divide_set, trainingSet=[] , testSet=[]):
 def getNeighbors(trainingSet, testInstance, k):
 	distances = []
 	length = len(testInstance)-1#porque é um vetor
+
 
     #Fará o calculo euclidiano de distancia entre os n valores teste 
 	for x in range(len(trainingSet)):
@@ -36,11 +41,53 @@ def getNeighbors(trainingSet, testInstance, k):
 		neighbors.append(distances[x][0])#para o numero k de vizinhos, varre a matriz e obtém as instancias de vizinhos
 	return neighbors
 
+
+#calculo de distancia eucliadiana
+def euclideanDistance(instance1, instance2, length):
+	distance = 0
+	for x in range(length):
+		distance += pow((instance1[x] - instance2[x]), 2)
+	return math.sqrt(distance)
+
+
+#Define a classe tendo como base os vizinhos
+def getResponse(neighbors):
+	classVotes = {}#Cria um Dict(Tipo um JSON)
+	for x in range(len(neighbors)):#le ate o ultimo vizinho
+		response = neighbors[x][-1]#armazena de cada linha o ultimo valor
+		if response in classVotes:#se o valor armazenado pertence a um dict, incrementa esse valor
+			classVotes[response] += 1
+		else:#senão, cria um novo dict e define que existe 1 instancia
+			classVotes[response] = 1
+	sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)# Ordena os valores do maior pro menor
+	return sortedVotes[0][0]
+
+
+#verifica a precisao dos dados
+def getAccuracy(testSet, predictions):
+	correct = 0
+	for x in range(len(testSet)):
+		if testSet[x][-1] is predictions[x]: #se o test for igual a previsao, incrementa 1 aos corretos
+			correct += 1
+	return (correct/float(len(testSet))) * 100.0 #divide os corretos pelo total e transforma em porcentagem
+
 def main():
+    classVotes = {}
     trainingSet=[]
     testSet=[]
     loadDataset('DataCSV\iris.csv', 0.66, trainingSet, testSet)
     print ('Train: ' + repr(len(trainingSet)))
     print ('Test: ' + repr(len(testSet)))
+
+    #trainSet = [[2, 2, 2, 'a'], [4, 4, 4, 'b'],[3,3,3,'a']]
+    testInstance = [0.78, 0.7,0.93]
+    k = 8
+    neighbors = getNeighbors(loadDataset('DataCSV\iris.csv', 0.66, trainingSet, testSet), testInstance, k)
+    print(neighbors)
+
+    nyan = getResponse(neighbors)
+
+    print(nyan)
+
 
 main()
