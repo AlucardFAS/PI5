2f3e308dd40baf98c81e9165daa5685a5b0896e7
DaniloAM <daniloam_a7x@hotmail.com>
2018-05-06 15:47:27 -0300

    Update search.py

--- PI V/Projeto Final/animeSearch/search.py
+++ PI V/Projeto Final/animeSearch/search.py
@@ -10,7 +10,7 @@ url = "https://myanimelist.net/anime/"
 
 file = open("test.txt", "w", encoding="utf-8")
 
-for x in range(11750, 11759) :
+for x in range(1, 10000) :
 
     try:
         page = urllib.request.urlopen(url + str(x))
@@ -21,6 +21,25 @@ for x in range(11750, 11759) :
     else:
         soup = BeautifulSoup(page,"html.parser")
         spanTagDark = soup.find_all("span", {"class": "dark_text"})
+
+##Status 
+
+        shouldContinue = 0
+    
+        for tag in spanTagDark:
+            if 'Status:' in tag:
+                status = tag.parent
+                status = str(status)
+                statusLine = status.split("\n")
+                status = statusLine[2]
+                status = dedent(status)
+                if status == "Not yet aired":
+                    shouldContinue = 1
+
+        print("Status: " + status)
+        
+        if shouldContinue > 0:
+            continue
         
 ##title anime
         print("/////////////////////////////////////////////////////////////////////////////")
@@ -34,80 +53,117 @@ for x in range(11750, 11759) :
         print(idstr)
 
 ##episodes
-        episode = soup.find('span', id="curEps").string
-        print ("Episodios: " + episode) 
+        try: 
+            episode = soup.find('span', id="curEps").string
+            print ("Episodios: " + episode) 
 
 ##Score
-        ratingValue = soup.find('span', itemprop="ratingValue").string
-        print ("Pontuacao/Nota: " + ratingValue) 
+            ratingValue = soup.find('span', itemprop="ratingValue").string
+            print ("Pontuacao/Nota: " + ratingValue) 
 
 ##number of members rating
-        ratingCount = soup.find('span', itemprop="ratingCount").string
-        ratingCount = ratingCount.replace(",", "")
-        print ("Numero de votos: " + ratingCount)
+            ratingCount = soup.find('span', itemprop="ratingCount").string
+            ratingCount = ratingCount.replace(",", "")
+            print ("Numero de votos: " + ratingCount)
 
+
+##Producer
+            
+            for tag in spanTagDark:
+                if 'Producers:' in tag:
+                    producer = tag.parent
+            
+            producer = str(producer)
+            producerLine = producer.split("\n")
+            producer = producerLine[2]
+            producer = dedent(producer)
+            split = producer.split("title=\"")
+            endIndex = split[1].find("\">")
+            producer = split[1][:endIndex]
+            producer = producer.replace(" ", replace)
+            print("Produtor: " + producer)
+            
 ##Studio
-        studio = searchStudio.studioSearch((""),soup)
-        studio = studio.replace(" ", replace)
-        print ("Estudio: " + studio)
+            studio = searchStudio.studioSearch((""),soup)
+            studio = studio.replace(" ", replace)
+            print ("Estudio: " + studio)
 
 ##rating
         
-        for tag in spanTagDark: #search for rating in span = dark_text
-            if 'Rating:' in tag:
-                rating = tag.parent
+            for tag in spanTagDark: #search for rating in span = dark_text
+                if 'Rating:' in tag:
+                    rating = tag.parent
 
 #Source
 
-        for tag in spanTagDark:
-            if 'Source:' in tag:
-                source = tag.parent
-
-        source = str(source)
-        sourceLine = source.split("\n")
-        source = sourceLine[2]
-        source = dedent(source)
-        source = source.replace(" ", replace)
-        print("Origem: " + source)
-
+            for tag in spanTagDark:
+                if 'Source:' in tag:
+                    source = tag.parent
+    
+            source = str(source)
+            sourceLine = source.split("\n")
+            source = sourceLine[2]
+            source = dedent(source)
+            source = source.replace(" ", replace)
+            print("Origem: " + source)
+    
 #Genres
 
-        for tag in spanTagDark:
-            if 'Genres:' in tag:
-                genres = tag.parent
+            for tag in spanTagDark:
+                if 'Genres:' in tag:
+                    genres = tag.parent
 
         #print(genres)
 
-        genres = str(genres)
-        firstIndex = genres.find("")
+            genres = str(genres)
+            firstIndex = genres.find("")
 
-        firstGenre = secondGenre = thirdGenre = ""
+            firstGenre = secondGenre = thirdGenre = ""
         
-        split = genres.split("title=\"")
+            split = genres.split("title=\"")
 
-        if len(split) >= 1:
-            firstEnd = split[1].find("\"")
-            firstGenre = split[1][:firstEnd]
+            if len(split) >= 1:
+                firstEnd = split[1].find("\"")
+                firstGenre = split[1][:firstEnd]
 
-        if len(split) >= 2: 
-            secondEnd = split[2].find("\"")
-            secondGenre = split[2][:secondEnd]
+            if len(split) >= 2: 
+                secondEnd = split[2].find("\"")
+                secondGenre = split[2][:secondEnd]
 
-        if len(split) >= 3: 
-            thirdEnd = split[3].find("\"")
-            thirdGenre = split[3][:thirdEnd]
+            if len(split) >= 3: 
+                thirdEnd = split[3].find("\"")
+                thirdGenre = split[3][:thirdEnd]
 
-        print("Generos: " + firstGenre + ", " + secondGenre + ", " + thirdGenre)
+            print("Generos: " + firstGenre + ", " + secondGenre + ", " + thirdGenre)
         
+##Duration
+            
+            for tag in spanTagDark:
+                if 'Duration:' in tag:
+                    duration = tag.parent
+    
+            duration = str(duration)
+            durationLine = duration.split("\n")
+            duration = durationLine[2]
+            duration = dedent(duration)
+            duration = duration.replace(" ", replace)
+            print("Duração: " + duration)
 
 #exclude code(div and scan), normalize rating in string(less indent)
-        rating = str(rating)
-        ratingLine = rating.split("\n")
-        rating = ratingLine[2]
-        rating = dedent(rating)
-        rating = rating.replace(" ", replace)
-        print("Classificacao: " + rating)
-        file.write(title + ", " + studio + ", " + rating + ", " + episode + ", " + ratingCount + ", " + ratingValue)
+            rating = str(rating)
+            ratingLine = rating.split("\n")
+            rating = ratingLine[2]
+            rating = dedent(rating)
+            rating = rating.replace(" ", replace)
+            print("Classificacao: " + rating)
+
+
+            file.write(str(x) + ", " + status + ", " + producer + ", " + studio + ", " + source + ", " + firstGenre + ", " + secondGenre + ", " + thirdGenre + ", " + duration + ", " + rating + ", " + episode + ", " + ratingValue + "\n")
+
+
+        except:
+            print("Can't find all the info from the anime")
+            continue
 
 file.close()
 
