f4e4747bafd77e79ef417d2f1685f3f03fdf68ac
Fernando A da Silva <FernandoImp@outlook.com.br>
2018-05-05 21:00:13 -0300

    Busca classificação indicativa
    Agora o código busca um total de 6 parametros

--- PI V/Projeto Final/animeSearch/search.py
+++ PI V/Projeto Final/animeSearch/search.py
@@ -1,13 +1,15 @@
 import urllib2
 import searchStudio
+from textwrap import dedent
 from bs4 import BeautifulSoup
 
 
-anime = "https://myanimelist.net/anime/136/Hunter_x_Hunter"
+anime = "https://myanimelist.net/anime/1000"
 page = urllib2.urlopen(anime)
 soup = BeautifulSoup(page,"html.parser")
+spanTagDark = soup.find_all("span", {"class": "dark_text"})
 
-##print soup.prettify() to test correctly call page code
+#print soup.prettify() #to test correctly call page code
 
 
 
@@ -19,14 +21,28 @@ print title
 episode = soup.find('span', id="curEps").string
 print ("Episodios: " + episode) 
 
-##rating
+##Score
 ratingValue = soup.find('span', itemprop="ratingValue").string
-print ("Classificacao: " + ratingValue) 
+print ("Pontuacao/Nota: " + ratingValue) 
 
 ##number of members rating
 ratingCount = soup.find('span', itemprop="ratingCount").string
-print ("Numero de classificadores: " + ratingCount) 
+print ("Numero de votos: " + ratingCount) 
 
 ##Studio
 studio = ""
-print ("Estudio: " + searchStudio.studioSearch(studio,soup))
+print ("Estudio: " + searchStudio.studioSearch((studio),soup))
+
+##rating
+for tag in spanTagDark: #search for rating in span = dark_text
+    if 'Rating:' in tag:
+        rating = tag.parent
+
+#exclude code(div and scan), normalize rating in string(less indent)
+rating = str(rating)
+ratingLine = rating.split("\n")
+rating = ratingLine[2]
+rating = dedent(rating)
+
+print("Classificacao: " + rating)
+
