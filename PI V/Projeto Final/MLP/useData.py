3563beb7e2eafbeed51973d8eda1fe5d22c793d7
Fernando A da Silva <FernandoImp@outlook.com.br>
2018-06-03 19:04:49 -0300

    erro pra inserir instância
    ValueError: Expected 2D array, got 1D array instead:
    array=[1.0e+00 4.0e+00 3.0e+00 1.4e+01 1.0e+00 3.0e+00 1.0e+00 1.0e+00 1.0e+00
     1.0e-02].
    Reshape your data either using array.reshape(-1, 1) if your data has a single feature or array.reshape(1, -1) if it contains a single sample.

--- PI V/Projeto Final/MLP/useData.py
+++ PI V/Projeto Final/MLP/useData.py
@@ -2,35 +2,46 @@ import numpy
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics.scorer import make_scorer
+from sklearn.metrics import recall_score
+from sklearn import svm
 
-def MLP_use(data):
+def cross(data):
     df_x = data.iloc[:,1:] #Matriz
     df_y = data.iloc[:,10] #Classes
 
-    x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size = 0.2, random_state = 4)
-    mlp = MLPClassifier(activation='logistic', solver='sgd', hidden_layer_sizes=(10,15), random_state=1)
-    mlp.fit(x_train, y_train)
-    pred = mlp.predict(x_test)
+    clf = svm.SVC(kernel='linear', C=1) #Name: 10, Length: 3020, dtype: int64
+    scores = cross_val_score(clf, df_x, df_y, cv=10)
+    predicted = cross_val_predict(clf, df_x, df_y, cv=10)
 
-    a = y_test.values
-    count = 0
+    print("Cross-Validation Score: ")
+    for fold in range(len(scores)):
+        print(str(fold+1) + "-K Fold Precision: " + str(scores[fold]))
+    
+    print("Cross-Validation Predicts: ")
+    print(predicted)
 
-    for i in range(len(pred)):
-        if pred[i] == a[i]:
-            count = count+1
+def predictClass(testInstance,data):
+    df_x = data.iloc[:,0:9] #Matriz
+    df_y = data.iloc[:,10] #Classes
 
-    print('Acertos: ')
-    print(count)
-    print('Quantidade de testes: ')
-    print(len(pred))
-    print('Precisão: ')
-    print((100*(count/len(pred))))
+    print(df_x)
+
+    mlp = MLPClassifier(activation='logistic', solver='sgd', hidden_layer_sizes=(10,15))
+    mlp.fit(df_x,df_y)
+
+    print(mlp.predict(testInstance))
+
     
 def main():
 
     animes_pd = pd.read_csv('input\\anime.csv', encoding = "ISO-8859-1")
-    MLP_use(animes_pd)
+    testInstance = []
+    testInstance = [1,4,3,14,1,3,1,1,1,0.01] 
+    #cross(animes_pd)
+    predictClass(testInstance,animes_pd)
 
 
 
