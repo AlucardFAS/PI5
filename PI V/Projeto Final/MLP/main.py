0593c2f7143bb6ff8816ae0121c749ca97dab31d
Victor Eleuterio Trindade <eleuteriotrindade@gmail.com>
2018-06-15 16:34:53 -0300

    Att code!!

---
+++ PI V/Projeto Final/MLP/main.py
@@ -0,0 +1,12 @@
+import useData
+
+def main():
+
+    mlp = newMLP()
+
+    testInstance = [[20,4,12,1,4,4]]
+
+#    mlp,df_x,df_y = predictAll(mlp,df_x,df_y)
+
+
+main()
--- PI V/Projeto Final/MLP/useData.py
+++ PI V/Projeto Final/MLP/useData.py
@@ -11,14 +11,35 @@ from sklearn import svm
 
 def predictClass(testInstance,mlp,df_x,df_y):
 
+#Prediz uma única classe de instância(nota)
+#Retorna o valor da classe
+
     df_x.drop(df_x.columns[[5,4,3]], axis=1, inplace=True)
     mlp.fit(df_x,df_y)
 
     prevision = mlp.predict(testInstance)
 
     return prevision
+
+def predictAll(mlp,df_x,df_y):
+
+#Prediz classe, gera score e probabilidade de cada classe
+#Retorna a probabilidade de cada classe
+
+    print('Resposta da instância teste:')
+    print(predictClass(testInstance,mlp,df_x,df_y))
+    print('Precisão média nos dados e rótulos de teste fornecidos')
+    print(mlp.score(df_x,df_y))
+    print('Estimativas de probabilidade de cada classe:')
+    proba = mlp.predict_proba(df_x)
+    print(proba[11])
+
+    return proba
     
-def main():
+def newMLP():
+
+#Cria o MLP
+#Retorna o MLP, matriz de instâncias e coluna de notas
 
     mlp = MLPClassifier(hidden_layer_sizes=(10,15), solver='sgd',max_iter=600, random_state=1)
     animes_pd = pd.read_csv('input\\anime.csv', encoding = "ISO-8859-1")
@@ -28,16 +49,9 @@ def main():
 
     #id,current,producer,studio,font,generos[3],duracao,classificacao,n ep
     #test recebe = producer,studio,font,duracao,classificacao,n ep
-    testInstance = [[20,4,12,1,4,4]]
+    #testInstance = [[20,4,12,1,4,4]]
+
+    print('MLP Criado com sucesso')
     
-    print('Resposta da instância teste:')
-    print(predictClass(testInstance,mlp,df_x,df_y))
-    print('Precisão média nos dados e rótulos de teste fornecidos')
-    print(mlp.score(df_x,df_y))
-    print('Estimativas de probabilidade de cada classe:')
-    proba = mlp.predict_proba(df_x)
-    print(proba[11])
+    return mlp,df_x,df_y
 
-
-
-main()
