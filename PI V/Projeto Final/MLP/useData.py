1e0ced2c42cf42bba9afd98e200821ac8ccbcdfe
Fernando A da Silva <FernandoImp@outlook.com.br>
2018-06-15 00:31:44 -0300

    Prediz e demonstra precisões

--- PI V/Projeto Final/MLP/useData.py
+++ PI V/Projeto Final/MLP/useData.py
@@ -9,54 +9,34 @@ from sklearn.metrics.scorer import make_scorer
 from sklearn.metrics import recall_score
 from sklearn import svm
 
-def predictClass(testInstance,data):
-    df_x = data.iloc[:,2:11] #Matriz
-    df_y = data.iloc[:,11] #Classes
+def predictClass(testInstance,mlp,df_x,df_y):
 
-    matrix = df_x.values.T.tolist()
-
-    third = matrix.pop(7)
-    second = matrix.pop(6)
-    first = matrix.pop(5)
-
-    genresList = []
-
-    #for x in range(0, len(first)):
-        
-        #genres = []
-
-        #if re.search('[a-zA-Z]', first[x]) is None:
-         #   genres.append(int(first[x]))
-        #if re.search('[a-zA-Z]', second[x]) is None:
-        #    genres.append(int(second[x]))
-       # if re.search('[a-zA-Z]', third[x]) is None:
-        #    genres.append(int(third[x]))
-
-       # genresList.append(genres)
-
-    
     df_x.drop(df_x.columns[[5,4,3]], axis=1, inplace=True)
-    #print(genres)
-    print(df_x)
-
-    #series = pd.Series(genresList)
-    #df_x['genres'] = series.values 
-
-    #print(df_x)
-
-    mlp = MLPClassifier(activation='logistic', solver='sgd', hidden_layer_sizes=(10,15))
     mlp.fit(df_x,df_y)
 
-    print(mlp.predict(testInstance))
+    prevision = mlp.predict(testInstance)
 
+    return prevision
     
 def main():
 
+    mlp = MLPClassifier(hidden_layer_sizes=(10,15), solver='sgd',max_iter=600, random_state=1)
     animes_pd = pd.read_csv('input\\anime.csv', encoding = "ISO-8859-1")
-    testInstance = [[1,1,1,4,1,1]]
-    #cross(animes_pd)
-    predictClass(testInstance,animes_pd)
 
+    df_x = animes_pd.iloc[:,2:11] #Matriz
+    df_y = animes_pd.iloc[:,11] #Classes
+
+    #id,current,producer,studio,font,generos[3],duracao,classificacao,n ep
+    #test recebe = producer,studio,font,duracao,classificacao,n ep
+    testInstance = [[20,4,12,1,4,4]]
+    
+    print('Resposta da instância teste:')
+    print(predictClass(testInstance,mlp,df_x,df_y))
+    print('Precisão média nos dados e rótulos de teste fornecidos')
+    print(mlp.score(df_x,df_y))
+    print('Estimativas de probabilidade de cada classe:')
+    proba = mlp.predict_proba(df_x)
+    print(proba[11])
 
 
 
